709183d3f541c5f3d0e8b4eac529b042cea33453
thamizhkko <thamizhkko8@gmail.com>
2025-04-18 14:40:22 +0530

    1st commit

--- src/main/java/com/example/Spring_Cinema_Producer/Service/CinemaService.java
+++ src/main/java/com/example/Spring_Cinema_Producer/Service/CinemaService.java
@@ -3,6 +3,8 @@ package com.example.Spring_Cinema_Producer.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +14,21 @@ import com.example.Spring_Cinema_Producer.Repository.CinemaRepository;
 @Service
 public class CinemaService {
 
+	// ✅ Log4j2 Logger initialization
+    private static final Logger logger = LogManager.getLogger(CinemaService.class);
+
 	@Autowired
 	CinemaRepository cinemaRepo;
 	
 	public List<CinemaDTO> cinemaList(){
+		 logger.info("Fetching list of cinemas from repository"); // ✅ Info log before operation
 		
-		return cinemaRepo.findAll().stream()
+		List<CinemaDTO> cinemas= cinemaRepo.findAll().stream()
 				.map(cinema->new CinemaDTO(cinema.getId(), cinema.getName(), cinema.getLocation()))
 				.collect(Collectors.toList());
+		
+		 logger.info("Number of cinemas fetched: {}", cinemas.size());
+		return cinemas;
 	}
 	
 }
